55fd8f4e31f39088a28104e92493b12370696011
sabrinaporto <sabrinapmota@yahoo.com.br>
2022-10-18 15:40:00 -0300

    início do cadastro do doador

--- doacao-pontos/src/main/java/com/api/doacaopontos/model/CadastroDoadorModel.java
+++ doacao-pontos/src/main/java/com/api/doacaopontos/model/CadastroDoadorModel.java
@@ -1,4 +1,38 @@
 package com.api.doacaopontos.model;
 
-public class CadastroDoadorModel {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "TB_DOADOR")
+public class CadastroDoadorModel implements Serializable {
+    private static final Long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, length = 30)
+    private String nome;
+    @CPF
+    private String cpf;
+    @Email
+    private String email;
+    @Column(nullable = false, length = 12)
+    private String telefone;
+    @Column(nullable = false, length = 250)
+    private String endereco;
+    @Column(nullable = false, length = 40)
+    private String bairro;
+    @Column(nullable = false, length = 8)
+    private String cep;
+    private String pontos;
 }

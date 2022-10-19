5921bc4051ad878c91ece170feb8c4c6cea8db4c
MaxSouza88 <souza1988@hotmail.com>
2022-10-19 14:10:44 -0300

    Atualizando projeto

--- doacao-pontos/src/main/java/com/api/doacaopontos/repository/RecolhaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.doacaopontos.repository;
-
-import com.api.doacaopontos.model.RecolhaModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecolhaRepository extends JpaRepository<RecolhaModel, Long> {
-}

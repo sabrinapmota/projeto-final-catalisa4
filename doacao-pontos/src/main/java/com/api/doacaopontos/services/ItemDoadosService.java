cc3e60b78472260b93e2ea350d3ef480fd11215a
will2375 <wil.liam_benicio@hotmail.com>
2022-10-20 16:03:21 -0300

    metodos de pontos

--- doacao-pontos/src/main/java/com/api/doacaopontos/services/ItemDoadosService.java
+++ doacao-pontos/src/main/java/com/api/doacaopontos/services/ItemDoadosService.java
@@ -30,6 +30,7 @@ public class ItemDoadosService {
     public ItemDoado cadastrar(ItemDoado itemDoado) {
         itemDoado.setIdPessoaDoadora(itemDoado.getUsuarioModel().getId());
         itemDoado.setStatus("ABERTO");
+        itemDoado.setDataInicio(LocalDate.now());
         return itemDoadoRepository.save(itemDoado);
     }
 

355c9b206bf520ce66f51df856e28a19b2f9bad1
MaxSouza88 <souza1988@hotmail.com>
2022-10-26 15:30:22 -0300

    Atualizando projeto

---
+++ Front-ProjetoFinal/js/consultaUsuario.js
@@ -0,0 +1,24 @@
+const form = document.getElementById("table-consulta")
+
+//table - trazer usuários cadastrados na tela
+fetch("http://localhost:8080/usuarios").then((data)=>{
+    return data.json();
+  }).then((todosUsuarios) =>{
+      let data1 = "";
+      todosUsuarios.map((values) =>{
+          data1 += `
+          <tbody>
+    
+              <tr align="center">
+                  <th scope="row">${values.id} </td>
+                  <td>${values.nome}</td> 
+                  <td>${values.email}</td> 
+                  <td>${values.telefone} </td>
+                  <td>${values.pontos}</td> 
+              </tr>
+          </tbody>
+          `
+      })
+      document.getElementById("usuarioDados").innerHTML = data1;
+      console.log(todosUsuarios);
+  })
